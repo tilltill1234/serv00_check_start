05cad69fbc4afee863ada5b0f119c92f62237688
dream <1045335841@qq.com>
2024-09-14 09:40:28 +0800

    Update check_and_start.sh

--- check_and_start.sh
+++ check_and_start.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# 填写自己的端口号
-PORT=1977
+# 端口号
+PORT=【你自己的TCP端口】
 
-# 检查端口是否在监听，存在跳过，不存在执行安装老王脚本
-if sockstat -l | grep -q ":$PORT"; then
-    echo "Port $PORT is already in use. No action needed."
+# 检查端口是否在监听
+PORT_IN_USE=$(sockstat -l | grep -q ":$PORT"; echo $?)
+
+# 检查Argo进程是否在运行（通过检查 COMMAND 列是否包含 tunnel）
+Argo_RUNNING=$(ps aux | grep "[t]unnel" ; echo $?)
+
+# 如果端口不在使用或Argo不在运行，则执行安装脚本
+if [ $PORT_IN_USE -ne 0 ] || [ $Argo_RUNNING -ne 0 ]; then
+    echo "Port $PORT is not in use or Argo is not running. Starting Vmess..."
+
+    # 设置TCP端口、ARGO_AUTH、ARGO_DOMAIN并启动老王Vmess脚本
+    PORT=【你自己的TCP端口】 ARGO_AUTH=【你自己的Argo隧道密钥】 ARGO_DOMAIN=【你自己的隧道域名】 bash <(curl -Ls https://raw.githubusercontent.com/eooce/scripts/master/containers-shell/00_vmess.sh)
 else
-    echo "Port $PORT is not in use. Starting Hysteria2 or Vmess..."
-    PORT=$PORT bash /home/tilltill1234/2.sh
+    echo "Port $PORT is in use and Argo is already running. No action needed."
 fi
-
-# 远程执行 check_and_start.sh 脚本检查S9
-echo "Connecting to s9.serv00.com to execute check_and_start.sh..."
-ssh till@s9.serv00.com 'bash /home/till/check_and_start.sh'
